925dd0097ccb7e592b7ff441c955536fa356b9b6
gnasri <ghassen.nasri@marklogic.com>
2019-12-19 08:59:03 +0100

    Initial commit of spring boot project

--- src/main/java/com/marklogic/mock/config/BatchConfig.java
+++ src/main/java/com/marklogic/mock/config/BatchConfig.java
@@ -10,43 +10,26 @@ import com.marklogic.client.io.StringHandle;
 import com.marklogic.client.io.marker.AbstractWriteHandle;
 import com.marklogic.client.io.marker.DocumentMetadataWriteHandle;
 import com.marklogic.mock.model.Employee;
-import com.marklogic.mock.processor.EmployeeItemProcessor;
 import com.marklogic.spring.batch.item.processor.MarkLogicItemProcessor;
 import com.marklogic.spring.batch.item.writer.MarkLogicItemWriter;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.xml.StaxEventItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.oxm.xstream.XStreamMarshaller;
-
 
 import javax.sql.DataSource;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 
@@ -159,81 +142,7 @@ public class BatchConfig {
                 .build();
     }
 
-   /* @Bean
-    public EmployeeItemProcessor processor(){
-        return new EmployeeItemProcessor();
-    }
 
-   @Bean
-    ItemWriter<Employee> databaseXmlItemWriter(Environment environment) {
-        StaxEventItemWriter<Employee> xmlFileWriter = new StaxEventItemWriter<>();
-
-        String exportFilePath = environment.getRequiredProperty(PROPERTY_XML_EXPORT_FILE_PATH);
-        xmlFileWriter.setResource(new FileSystemResource(exportFilePath));
-
-        xmlFileWriter.setRootTagName("Employees");
-
-        Jaxb2Marshaller studentMarshaller = new Jaxb2Marshaller();
-        studentMarshaller.setClassesToBeBound(Employee.class);
-        xmlFileWriter.setMarshaller(studentMarshaller);
-
-        return xmlFileWriter;
-    }
-
-    @Bean
-   public StaxEventItemWriter<Employee> writer(){
-        StaxEventItemWriter<Employee> writer = new StaxEventItemWriter<Employee>();
-        writer.setResource(new ClassPathResource("persons.xml"));
-
-        Map<String,String> aliasesMap =new HashMap<String,String>();
-        aliasesMap.put("employee", "com.marklogic.mock.model.Employee");
-        XStreamMarshaller marshaller = new XStreamMarshaller();
-        try {
-            marshaller.setAliases(aliasesMap);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        writer.setMarshaller(marshaller);
-        writer.setRootTagName("Employees");
-        writer.setOverwriteOutput(true);
-        return writer;
-    }
-
-   @Bean
-    public Step step1(){
-        return stepBuilderFactory.get("step1").<Employee,Employee>chunk(100).reader(reader()).processor(processor()).writer(writer()).build();
-    }
-
-    @Bean
-    public Job exportPerosnJob(){
-        return jobBuilderFactory.get("exportPeronJob").incrementer(new RunIdIncrementer()).flow(step1()).end().build();
-    }
-   @Bean
-   ItemProcessor<Employee, Employee> databaseXmlItemProcessor() {
-       return new EmployeeItemProcessor();
-   }
-   @Bean
-   Step databaseToXmlFileStep(ItemReader<Employee> jdbcCursorItemReader,
-                              ItemProcessor<Employee, Employee> databaseXmlItemProcessor,
-                              ItemWriter<Employee> databaseXmlItemWriter,
-                              StepBuilderFactory stepBuilderFactory) {
-       return stepBuilderFactory.get("databaseToXmlFileStep")
-               .<Employee, Employee>chunk(1)
-               .reader(jdbcCursorItemReader)
-               .processor(databaseXmlItemProcessor)
-               .writer(databaseXmlItemWriter)
-               .build();
-   }
-
-    @Bean
-    Job databaseToXmlFileJob(JobBuilderFactory jobBuilderFactory,
-                             @Qualifier("databaseToXmlFileStep") Step csvStudentStep) {
-        return jobBuilderFactory.get("databaseToXmlFileJob")
-                .incrementer(new RunIdIncrementer())
-                .flow(csvStudentStep)
-                .end()
-                .build();
-    }*/
    @Bean
    public Job job(JobBuilderFactory jobBuilderFactory, Step step) {
 
